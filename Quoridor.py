09b99e5eed73a498490599eee301a30ba8c17c8b
schmkelsosu <schmkels@oregonstate.edu>
2021-07-28 19:52:58 -0500

    initial file creation

---
+++ Quoridor.py
@@ -0,0 +1,20 @@
+# Author: Kelsey Schmidt
+# Date: 7-28-21
+# Description:  # The "Description" comment should be a high-level description
+                # of what the code in the file does.
+
+class QuoridorGame:
+    """
+
+    """
+    pass
+
+
+def main():
+    """
+    A main function to run the program as a script or a module.
+    """
+    pass
+
+if __name__ == '__main__':  # if running as a script, runs the main function
+  main()
